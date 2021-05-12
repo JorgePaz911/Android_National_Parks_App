4e265bb26b450e44205420d715a9b314a46ee338
JorgePaz911 <57108198+JorgePaz911@users.noreply.github.com>
2021-05-11 22:13:26 -0700

    Update Util.java

--- app/src/main/java/com/example/nationalparks/util/Util.java
+++ app/src/main/java/com/example/nationalparks/util/Util.java
@@ -3,9 +3,9 @@ package com.example.nationalparks.util;
 
 public class Util {
 
-    //public static final String PARKS_URL = "https://developer.nps.gov/api/v1/parks?stateCode=AZ&api_key=OUfbb6ulnXChgPDFHxCCeAwQoETt5xCbzx0mrf1Y";
+    //public static final String PARKS_URL = "https://developer.nps.gov/api/v1/parks?stateCode=AZ&api_key=key";
 
     public static String getParksUrl(String stateCode){
-        return "https://developer.nps.gov/api/v1/parks?stateCode=" + stateCode + "&api_key=OUfbb6ulnXChgPDFHxCCeAwQoETt5xCbzx0mrf1Y";
+        return "https://developer.nps.gov/api/v1/parks?stateCode=" + stateCode + "key";
     }
 }
